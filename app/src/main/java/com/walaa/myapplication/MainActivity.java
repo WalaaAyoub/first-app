755bdc38c396a5e4018b32a5e284796ffe241c54
WalaaAyoub <ayoubwalaa4@gmail.com>
2024-09-15 13:09:04 +0300

    first task button and text

--- app/src/main/java/com/walaa/myapplication/MainActivity.java
+++ app/src/main/java/com/walaa/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.walaa.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
+    Button click;
+    Integer c=0;
+    String text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        text=findViewById(R.id.textView);
+        click=findViewById(R.id.button);
+        text1= (String) text.getText();
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                c++;
+                text.setText(text1+" "+c);
+            }
         });
     }
 }
